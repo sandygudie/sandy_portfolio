f404454bd470e383c7f3220b21a84197d6c95c89
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2022-12-28 16:21:27 +0100

    updated tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,jsx}", , "./components/**/*.{js,jsx}"],
+  content:["./pages/**/*.{js,jsx}", , "./components/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
