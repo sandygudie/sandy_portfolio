2bc11451f99e3b2573f0d4233cfab494402db89f
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2022-12-31 22:07:24 +0100

    added update on header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,25 @@
 import * as React from "react";
 import Image from "next/image";
-import profileImage from "../assets/images/";
+import profileLogo from "../assets/images/logo.svg";
 
 const Header = () => (
   <div className="p-6 bg-secondary fixed w-full md:px-6 md:py-6 xl:px-8 z-10">
     <div className="hidden md:flex justify-between items-center">
-     
-    <Image
-              src={profileImage}
+     <div  className="border-2 p-2 rounded-full border-primary ">
+     <Image
+              src={profileLogo}
               alt="Photo"
-              width={350}
-              height={350}
-              objectFit="cover"
-              className="rounded-full"
+              width={30}
+              height={25}
+              objectFit="contain"
+              className="border-2  rounded-full"
               quality={100}
             />
-      <a
-        className="rounded-md text-center p-2 font-bold border border-white bg-white w-24 text-secondary hover:bg-transparent hover:text-white"
-        href="https://docs.google.com/document/d/1TGO4P6XlqEZfvw5tUH4uoEaLKvWILtxAAinyAPIGC8U/edit?usp=sharing"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Resume
-      </a>
+     </div>
+   
+    
 
-      <div className="flex justify-between w-96">
+      <div className="flex justify-between ">
         <div className="flex justify-between text-base gap-x-8">
           <a className="hover:text-primary" href="#profile">
             {" "}
@@ -40,6 +35,14 @@ const Header = () => (
           <a className="hover:text-primary" href="#contact">
             Contact
           </a>
+          <a
+        className="rounded-md text-center p-2 font-bold border border-white bg-white w-24 text-secondary hover:bg-transparent hover:text-white"
+        href="https://docs.google.com/document/d/1TGO4P6XlqEZfvw5tUH4uoEaLKvWILtxAAinyAPIGC8U/edit?usp=sharing"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Resume
+      </a>
         </div>
       </div>
     </div>
