b0c10cfe1f9a1a7c35aeae459fe047ebedcc1e41
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2023-02-04 14:05:52 +0100

    fix error page

--- pages/404.js
+++ pages/404.js
@@ -6,8 +6,8 @@ export default function Custom404() {
     <div className="w-5/6 fixed left-2/4 top-2/4 text-2xl font-bold translate-x-2/4 translate-y-2/4">
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <Link  href="/" className="text-red-500 text-sm">
-          <a>  Return to <span className="text-primary">Home </span>Page</a>
+      <Link  href={"/"} className="text-red-500 text-sm">
+        Return to <span className="text-primary">Home </span>Page
         </Link>
     </div>
   );
