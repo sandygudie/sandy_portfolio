a2177e4719dd5ac8ac1e0160c527d4e6e48edb7b
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2023-02-08 14:02:36 +0100

    fixed link issue

--- pages/404.js
+++ pages/404.js
@@ -6,9 +6,12 @@ export default function Custom404() {
     <div className="w-5/6 fixed left-2/4 top-2/4 text-2xl font-bold translate-x-2/4 translate-y-2/4">
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <Link  href={"/"} className="text-red-500 text-sm">
-        Return to <span className="text-primary">Home </span>Page
-        </Link>
+      <Link href={"/"} className="text-red-500 text-sm">
+        <>
+          {" "}
+          Return to <span className="text-primary">Home </span>Page
+        </>
+      </Link>
     </div>
   );
 }
