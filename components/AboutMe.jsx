adb36b8fc38c4e053bebed895af0e753066845ce
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2023-11-27 16:43:41 +0100

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -16,9 +16,7 @@ export default function AboutMe() {
         </p> */}
         <p className="leading-[2rem]">
           My name is Goodnews Sandy, I am an experience fullstack devloper with
-          expertise in building innovative web applications that are user
-          friendly and interactive. With deep knowledge of user experience and
-          design patterns, I build applications using high-quality code that is
+          expertise in building innovative web applications. I build applications using high-quality codes that is
           easy to maintain and scalable. I always exceed my clients expectations
           and deliver exceptional results.
         </p>
@@ -28,9 +26,7 @@ export default function AboutMe() {
           on{" "}
           <a className="underline" href="sandygoody.medium.com">
             Medium
-          </a>
-          . I also mentor upcoming beginners to learn software development
-          skills.
+          </a>.
         </p>
         <div>
           <div className="flex-col md:flex-row flex items-start justify-between gap-8">
@@ -38,35 +34,28 @@ export default function AboutMe() {
               {" "}
               <h3 className="text-xl text-white font-medium mt-8">Services</h3>
               <p>Web Development</p>
-              <p>Unit Testing</p>
-              <p>Devops</p>
+              <p>Open Source</p>
               <p>Technical Writing</p>
               <p>Mentoring</p>
-              <p>SEO Optimation</p>
-              <p>Localisation</p>
-              <p>Community and </p>
-              <p>Speaking Engagement</p>
+              <p>SEO Optimization</p>
+
+    
             </div>
             <div className="leading-[2rem] basis-1/2 text-lg text-gray-100">
               <h3 className="text-xl text-white font-medium mt-8">
                 Technologies I Use
               </h3>
               <p className="my-2 text-lg">
-                HTML, CSS, Javascript, Python, Jquery, Typescript, React, Vue,
-                NextJs, Vite, Webpack, Templating-Engines(EJS, Handlebars), Quarto{" "}
+              React, Vue, NextJs, Vite, Webpack, HTML, CSS, Jquery, Javascript, Typescript, {" "}
               </p>
-              
               <p>
-                Sass, CSS-in-Js, Styled-Component, TailwindCSS, Chakara-UI, Material-UI, Twin macro, framer-motion
+                Sass, CSS-in-Js, Styled-Component, TailwindCSS, Chakara-UI, Material-UI, Framer-motion
               </p>
               <p>Figma, Canva, PhotoShop, Adobe</p>
               <p className="my-2 text-lg">
-                NodeJs, ExpressJs, NestJs, Firebase, Rest-API, Apollo-GraphQL{" "}
+                NodeJs, ExpressJs, NestJs, Firebase, GraphQL,MongoDB, MySQL, PostgreSQL{" "}
               </p>
-              <p>Npm/Yarn, i18n, Slack, Jira, Asana, Trello</p>
-              <p>Goggle Analytics, Lighthouse</p>
-              <p className="my-2 text-lg">MongoDB, MySQL, PostgreSQL</p>
-              <p className="my-2 text-lg">Github Actions, Docker, Cypress</p>
+              <p className="my-2 text-lg">Github Actions, Docker-Compose, Cypress</p>
             </div>
           </div>
         </div>
