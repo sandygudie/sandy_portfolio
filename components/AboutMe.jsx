a1c1ada4822504a15929adb0ba82b223ae934674
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2026-05-31 11:47:22 +0300

    Revise AboutMe component content and tech stack
    Updated personal introduction and core skills. Added Power BI and SharePoint to the tech stack.

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -9,13 +9,15 @@ export default function AboutMe() {
       </div>
       <div className="lg:pl-32 lg:pr-8 text-gray-100 !text-[1.2rem] md:!text-[23px] !leading-[3.5rem]">
         <p className="">
-          My name is Goodnews Sandy, I am a software developer with
-          expertise in building innovative and scalable applications. I build efficient
-          digital solutions that help business and organizations achieve their goals.
+          I am a software developer with expertise in building innovative and scalable enterprise applications. I help businesses and organizations achieve their goals through reliable, efficient, and user-focused software solutions.
         </p>
 
         <p className="">
-          I contribute to open source communities and support digital transformation projects. I enjoy writing, check out my articles on{" "}
+          I contribute to open source communities and advocate for open technology because it benefits everyone and drives transparency and inclusive innovation.
+          <br></br>
+          I am currently working on initiatives that advance Africa’s digital future through technology, policy, and open collaboration, with a focus on digital transformation, interoperability, and impactful public sector innovation.
+          <br></br>
+          I also enjoy writing, check out my articles on{" "}
           <a className="underline text-white" target="_blank" rel="noreferrer" href="https://sandygoody.medium.com/">
             Medium
           </a>
@@ -27,12 +29,24 @@ export default function AboutMe() {
 
           <div className="flex-col md:flex-row flex items-center gap-x-8 py-4 md:py-6 justify-start basis-[60%] lg:basis-[40%] text-gray-100">
             <h3 className="text-xl md:text-2xl text-white w-64 font-medium">
+              Core Sills
+            </h3>
+            <div className="text-left !text-[1.2rem] md:!text-[23px] !leading-[3rem]">
+              <p>
+              Software development, Project Management, Data Analysis, Monitoring, Evaluation and Reporting, Data Management, Capacity building, Cybersecurity, 
+                Open Source technologies,
+              </p>
+            </div>
+          </div>
+          <div className="flex-col md:flex-row flex items-center gap-x-8 py-4 md:py-6 justify-start basis-[60%] lg:basis-[40%] text-gray-100">
+            <h3 className="text-xl md:text-2xl text-white w-64 font-medium">
               Tech Stack
             </h3>
             <div className="text-left !text-[1.2rem] md:!text-[23px] !leading-[3rem]">
               <p>
              JavaScript,
-                TypeScript, React, Vue, NextJs, Python, NodeJs, ExpressJs, NestJs, Redis, MongoDB, MySQL, Github Actions, Docker-Compose, Cypress
+                TypeScript, React, Vue, NextJs, Python, NodeJs, ExpressJs, NestJs, Redis, MongoDB, MySQL, Github Actions, Docker-Compose, Cypress, PowerBi,
+                Power Query, SharePoint
               </p>
             </div>
           </div>
