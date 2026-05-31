2f2e8cefa7e07cf58cc8a4d15c9dd66df1c4d064
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2026-05-31 12:38:41 +0300

    Update wording in AboutMe component

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
       <div className="lg:pl-32 lg:pr-8 text-gray-100 !text-[1.2rem] md:!text-[23px] !leading-[3.5rem]">
         <p className="">
           I am a software developer with expertise in building innovative and scalable enterprise applications. 
-          I help organizations achieve their goals through efficient, and user-focused software solutions. I advocate for open source technology because it benefits everyone and drives inclusive innovation.
+          I help organizations achieve their goals through efficient, and user-focused digital solutions. I advocate for open source technology because it benefits everyone and drives inclusive innovation.
         </p>
         <p className="mt-2">
           I am currently working on advancing Africa’s digital future through <span className="text-white">technology, </span><span className="text-white">policy, </span>and <span className="text-white">open collaboration</span>, with focus on impactful public sector innovation.
@@ -34,8 +34,7 @@ export default function AboutMe() {
             </h3>
             <div className="text-left !text-[1.2rem] md:!text-[23px] !leading-[3rem]">
               <p>
-             JavaScript,
-                TypeScript, React, Vue, NextJs, Python, NodeJs, ExpressJs, NestJs, Redis, MongoDB, MySQL, Github Actions, Docker-Compose, Cypress, PowerBi,
+             JavaScript, TypeScript, React, Vue, NextJs, Python, NodeJs, ExpressJs, NestJs, Redis, MongoDB, MySQL, Github Actions, Docker-Compose, Cypress, PowerBi,
                 Power Query, SharePoint
               </p>
             </div>
