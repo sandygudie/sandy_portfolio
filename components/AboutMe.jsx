0900152e0f9090a88c5d1637373cb65a35959947
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2026-05-31 12:07:30 +0300

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -9,24 +9,16 @@ export default function AboutMe() {
       </div>
       <div className="lg:pl-32 lg:pr-8 text-gray-100 !text-[1.2rem] md:!text-[23px] !leading-[3.5rem]">
         <p className="">
-          I am a software developer with expertise in building innovative and scalable enterprise applications. I help organizations achieve their goals through efficient, and user-focused software solutions.
+          I am a software developer with expertise in building innovative and scalable enterprise applications. 
+          I help organizations achieve their goals through efficient, and user-focused software solutions. I advocate for open source technology because it benefits everyone and drives inclusive innovation.
         </p>
-
+        <br></br>
+        <br></br>
         <p className="">
-          I advocate for open source technology because it benefits everyone and drives inclusive innovation.
-          <br></br>
-          I am currently working on advancing Africa’s digital future through technology, policy, and open collaboration, with focus on impactful public sector innovation.
-          <br></br>
-          I also enjoy writing, check out my articles on{" "}
-          <a className="underline text-white" target="_blank" rel="noreferrer" href="https://sandygoody.medium.com/">
-            Medium
-          </a>
-          .
+          I am currently working on advancing Africa’s digital future through <b>technology</b>,<b>policy</b>,and <b>open collaboration</b>, with focus on impactful public sector innovation.
         </p>
 
         <div className="mt-4 md:mt-8">
-          
-
           <div className="flex-col md:flex-row flex items-center gap-x-8 py-4 md:py-6 justify-start basis-[60%] lg:basis-[40%] text-gray-100">
             <h3 className="text-xl md:text-2xl text-white w-64 font-medium">
               Core Skills
