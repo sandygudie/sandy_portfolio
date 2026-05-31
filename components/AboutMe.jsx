69aab48a31eacb9d090b217857063b088cf0b561
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2026-05-31 12:00:30 +0300

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -9,13 +9,13 @@ export default function AboutMe() {
       </div>
       <div className="lg:pl-32 lg:pr-8 text-gray-100 !text-[1.2rem] md:!text-[23px] !leading-[3.5rem]">
         <p className="">
-          I am a software developer with expertise in building innovative and scalable enterprise applications. I help businesses and organizations achieve their goals through reliable, efficient, and user-focused software solutions.
+          I am a software developer with expertise in building innovative and scalable enterprise applications. I help organizations achieve their goals through efficient, and user-focused software solutions.
         </p>
 
         <p className="">
-          I contribute to open source communities and advocate for open technology because it benefits everyone and drives transparency and inclusive innovation.
+          I advocate for open source technology because it benefits everyone and drives inclusive innovation.
           <br></br>
-          I am currently working on initiatives that advance Africa’s digital future through technology, policy, and open collaboration, with a focus on digital transformation, interoperability, and impactful public sector innovation.
+          I am currently working on advancing Africa’s digital future through technology, policy, and open collaboration, with focus on impactful public sector innovation.
           <br></br>
           I also enjoy writing, check out my articles on{" "}
           <a className="underline text-white" target="_blank" rel="noreferrer" href="https://sandygoody.medium.com/">
@@ -29,12 +29,12 @@ export default function AboutMe() {
 
           <div className="flex-col md:flex-row flex items-center gap-x-8 py-4 md:py-6 justify-start basis-[60%] lg:basis-[40%] text-gray-100">
             <h3 className="text-xl md:text-2xl text-white w-64 font-medium">
-              Core Sills
+              Core Skills
             </h3>
             <div className="text-left !text-[1.2rem] md:!text-[23px] !leading-[3rem]">
               <p>
-              Software development, Project Management, Data Analysis, Monitoring, Evaluation and Reporting, Data Management, Capacity building, Cybersecurity, 
-                Open Source technologies,
+              Software development, Project management, Data analysis, Monitoring, evaluation and reporting, Data management, Capacity building, Cybersecurity, 
+                Open source technologies,
               </p>
             </div>
           </div>
